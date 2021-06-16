f9d5b0fed611dc89279dd942a485bf81a60cd398
yogibagus <58897819+yogibagus@users.noreply.github.com>
2021-06-16 16:05:24 +0700

    main

---
+++ test.js
@@ -0,0 +1,28 @@
+// 20210616120429
+// https://monitor.teamshrimp.com/api/fetch/all/139.99.125.54/7777/
+
+{
+  "error_code": 0,
+  "error_info": "Successful",
+  "online": true,
+  "password": 0,
+  "num_players": 493,
+  "max_players": 800,
+  "players": [
+    
+  ],
+  "servername": "Jogjagamers Reality Project",
+  "gametype": "MRP v3.16.3u5 On-Demand",
+  "map": "San Andreas",
+  "num_rules": 7,
+  "lagcomp": "On",
+  "mapname": "San Andreas",
+  "version": "0.3.DL-R1",
+  "weather": "13",
+  "weburl": "jogjagamers.org",
+  "worldtime": "12:00",
+  "country_code": null,
+  "country_name": null,
+  "ip": "139.99.125.54",
+  "port": 7777
+}
