ea54ae30cdc4d5dfc388485960aa42aa092acba2
yogibagus <58897819+yogibagus@users.noreply.github.com>
2021-10-08 15:45:31 +0700

    fix cmd down

--- commands/down.js
+++ commands/down.js
@@ -4,30 +4,30 @@ const samp = require("samp-query");
 const { serverip, port } = require('../config.json');
 
 module.exports = {
-	name: 'down',
-	description: 'Is Server Down?',
-	async execute(message, args) {
-		var status;
-		var settings = {
-			host: serverip,
-			port: port,
-		};
+    name: 'down',
+    description: 'Is Server Down?',
+    async execute(message, args) {
+        var status;
+        var settings = {
+            host: serverip,
+            port: port,
+        };
 
-		samp(settings, function (error, response) {
-			if (error) {
-				status = 'Tidak, status server sedang online.';
-			} else {
-				status = 'Ya, status server sedang offline';
-			}
-					//embed
-			const exampleEmbed = new Discord.MessageEmbed()
-				.setColor('#12570c')
-				.setAuthor('JGRP Bot', 'https://i.ibb.co/vw2GGKS/jg-logo.png', 'https://discord.js.org')
-				.addField('Apakah server down?', status, true)
-				.setTimestamp()
-				.setFooter('jogjagamers.org', 'https://i.ibb.co/vw2GGKS/jg-logo.png');
-		
-			message.channel.send(exampleEmbed);
-		})
-	},
+        samp(settings, function(error, response) {
+            if (error) {
+                status = 'Ya, status server sedang offline.';
+            } else {
+                status = 'Tidak, status server sedang online.';
+            }
+            //embed
+            const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#12570c')
+                .setAuthor('JGRP Bot', 'https://i.ibb.co/vw2GGKS/jg-logo.png', 'https://discord.js.org')
+                .addField('Apakah server down?', status, true)
+                .setTimestamp()
+                .setFooter('jogjagamers.org', 'https://i.ibb.co/vw2GGKS/jg-logo.png');
+
+            message.channel.send(exampleEmbed);
+        })
+    },
 };
